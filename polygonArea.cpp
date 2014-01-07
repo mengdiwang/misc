90f676e3c955cd9986b2ec59c43a6137cb952d21
Mengdi Wang <wangmengdi@gmail.com>
2014-01-07 19:26:51 +0800

    polygonArea

---
+++ polygonArea.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct TPoint
+{
+	TPoint(double ax, double ay)
+	{
+		x = ax;
+		y = ay;
+	}
+	
+	double x;
+	double y;
+};
+
+double polygonArea(TPoint points[], int n)
+{
+	double area = 0;
+
+	for(int i=1; i<=n; i++)
+	{
+		area += (points[i-1].x * points[i%n].y - points[i%n].x * points[i-1].y);
+	}
+	
+	return area/2;
+}
+
+int main()
+{	
+	TPoint points[4] = {TPoint(0,0), TPoint(1,0),TPoint(1,1), TPoint(0,1)};
+	double val = polygonArea(points, 4);
+	printf("%lf",val);
+
+	return 0;
+}
